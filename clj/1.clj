fd61240fa7970ea95eb56faa18e016df627ad83b
gtech <gtech@aeondynamics.com>
2011-01-12 20:27:48 -0800

    A few updates to 1.clj

--- clj/1.clj
+++ clj/1.clj
@@ -1,15 +1,26 @@
-(defn enumerate-multiples[max & divisors]
-    (loop [max multip divisors]
-      (when (= max 0) nil)
-      (if (some (= 0 (mod max %));;this isn't quite working yet
-		divisors))
-      (recur (dec max)
-	     (conj max multip)
-	     (divisors))
-      (recur (dec max)
-	     multip
-	     divisors)))
+(defn divisible?[number & divisors]
+  (loop [n number divs divisors]
+    (if (= 0
+	   (mod n
+		(first divs)))
+    true
+    (recur n
+	   (last divs)))))
 
+
+(defn enumerate-multiples[max & divisors]
+    (loop [cnt max, multip nil, divs divisors]
+      (when (= cnt 0)
+	nil)
+      (if (divisible? cnt
+		      divs)
+      (recur (dec cnt)
+	     (conj cnt multip)
+	     (divs))
+      (recur (dec cnt)
+	     multip
+	     divs))))
+   
 (defn sum-multiples[max & divisors]
   (apply + (enumerate-multiples max
 				divisors)))
