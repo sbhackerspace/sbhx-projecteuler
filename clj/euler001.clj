65dfa0da05aeec80d9a434a6b9db9ab66dcda688
Steve Phillips <elimisteve@gmail.com>
2011-01-12 22:49:34 -0800

    Turned euler001.clj into a more general solution as per gtech's recommendation

--- clj/euler001.clj
+++ clj/euler001.clj
@@ -1,6 +1,9 @@
 ; Steve Phillips / elimisteve
 ; 2011.01.12
 
-(println (apply + (filter #(or (= 0 (mod % 3))
-							   (= 0 (mod % 5)))
-						  (range 1000))))
+(defn sum-multiples [int1 int2 myrange]
+  (apply + (filter #(or (= 0 (mod % int1))
+						(= 0 (mod % int2)))
+				   (range myrange))))
+
+(println (sum-multiples 3 5 1000))
