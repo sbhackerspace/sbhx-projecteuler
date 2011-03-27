85c5b3dfa37b902a127e834975c5c4c606e24249
Steve Phillips <elimisteve@gmail.com>
2011-03-26 21:22:55 -0700

    Added Python and Ruby one-liners for #1

--- py/euler001.py
+++ py/euler001.py
@@ -1,24 +1,5 @@
 #!/usr/bin/python
-# 2009.04.02
+# 2009.04.02 (terrible version)
+# 2010.??.?? (one-liner)
 
-# Find the sum of all the multiples of 3 or 5 below 1000.
-
-## First try when I was first learning Python
-
-set = []
-mysum = 0
-
-for i in range(1000):
-    if i % 3 == 0 or i % 5 == 0:
-        set.append(i)
-
-for i in set:
-    mysum += i
-
-print mysum
-
-####
-
-## Idiomatic, functional solution
-
-print sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
+print sum([x for x in range(1000) if x%3 == 0 or x%5 == 0])
